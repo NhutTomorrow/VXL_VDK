1ad024e7e85dd391711d3d16206ebfb35b415b8f
NhutTomorrow <nhutmai0515@gmail.com>
2025-11-16 21:03:44 +0700

    error at SCH_Delete_Task

---
+++ LAB4/Scheduler/Core/Inc/main.h
@@ -0,0 +1,117 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f1xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "stdlib.h"
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define LED_RED_Signal_Pin GPIO_PIN_1
+#define LED_RED_Signal_GPIO_Port GPIOA
+#define GRE0_Pin GPIO_PIN_2
+#define GRE0_GPIO_Port GPIOA
+#define YEL0_Pin GPIO_PIN_3
+#define YEL0_GPIO_Port GPIOA
+#define RED0_Pin GPIO_PIN_4
+#define RED0_GPIO_Port GPIOA
+#define GRE1_Pin GPIO_PIN_5
+#define GRE1_GPIO_Port GPIOA
+#define YEL1_Pin GPIO_PIN_6
+#define YEL1_GPIO_Port GPIOA
+#define RED1_Pin GPIO_PIN_7
+#define RED1_GPIO_Port GPIOA
+#define SEG_0_Pin GPIO_PIN_0
+#define SEG_0_GPIO_Port GPIOB
+#define SEG_1_Pin GPIO_PIN_1
+#define SEG_1_GPIO_Port GPIOB
+#define SEG_2_Pin GPIO_PIN_2
+#define SEG_2_GPIO_Port GPIOB
+#define BTN4_Pin GPIO_PIN_11
+#define BTN4_GPIO_Port GPIOB
+#define BTN3_Pin GPIO_PIN_12
+#define BTN3_GPIO_Port GPIOB
+#define BTN0_Pin GPIO_PIN_13
+#define BTN0_GPIO_Port GPIOB
+#define BTN1_Pin GPIO_PIN_14
+#define BTN1_GPIO_Port GPIOB
+#define BTN2_Pin GPIO_PIN_15
+#define BTN2_GPIO_Port GPIOB
+#define EN0_Pin GPIO_PIN_8
+#define EN0_GPIO_Port GPIOA
+#define EN1_Pin GPIO_PIN_9
+#define EN1_GPIO_Port GPIOA
+#define EN2_Pin GPIO_PIN_10
+#define EN2_GPIO_Port GPIOA
+#define EN3_Pin GPIO_PIN_11
+#define EN3_GPIO_Port GPIOA
+#define SEG_3_Pin GPIO_PIN_3
+#define SEG_3_GPIO_Port GPIOB
+#define SEG_4_Pin GPIO_PIN_4
+#define SEG_4_GPIO_Port GPIOB
+#define SEG_5_Pin GPIO_PIN_5
+#define SEG_5_GPIO_Port GPIOB
+#define SEG_6_Pin GPIO_PIN_6
+#define SEG_6_GPIO_Port GPIOB
+#define SEG_7_Pin GPIO_PIN_7
+#define SEG_7_GPIO_Port GPIOB
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
